72fec195dc24c6d93f663b7bf432004d7c0a93b5
Skylar Senning <Skylar.Senning1@marist.edu>
2013-09-10 14:11:26 -0400

    First HW commit
    Query Homework

---
+++ CAP2 Query Homework.sql
@@ -0,0 +1,52 @@
+﻿--List all data	for all customers.
+SELECT name 
+FROM   agents
+WHERE  city = 'New York' OR city = 'Newark'
+
+--List all data	for all	customers.
+SELECT *
+FROM customers 
+
+--List the name	and city of agents named Smith.
+SELECT name, city 
+FROM agents 
+WHERE name = 'Smith'
+
+--List pid, name, and quantity	of products costing more than US$1.25.
+SELECT pid, name, quantity
+FROM products
+WHERE priceusd >= 1.25
+
+--List	the ordno and aid of all orders.
+SELECT ordno, aid 
+FROM orders 
+
+--List the names and cities of	customers not in Dallas.
+SELECT name, city
+FROM customers 
+WHERE city <>'Dallas'
+
+--List all data	for all	customers.
+SELECT *
+FROM customers
+
+--List all data for products not in New York or Newark that cost US$1 or less.
+SELECT *
+FROM products
+WHERE City NOT IN ('New York', 'Newark') AND priceusd <= 1
+
+--List all data	for orders in January or March
+SELECT *
+FROM orders
+WHERE mon = 'jan' OR mon = 'mar'  
+
+--List all data for orders in February less than US$100.
+SELECT *
+FROM orders 
+WHERE mon = 'feb' AND dollars < 100
+
+--List all orders from the customer whose cid is C005.
+SELECT *
+FROM  orders 
+WHERE cid = 'c005'
+
