e5c4e3e4111435c6206b4144018165d97e46425e
Skylar Senning <Skylar.Senning1@marist.edu>
2013-10-01 17:25:33 -0400

    Query Homework 3
    Working on the questions for monday…..not as easy as i thought……mostly
    done 1-8 with 4 needing work

---
+++ QueryHW3.sql
@@ -0,0 +1,126 @@
+-- 1. Get the cities of agents booking an order for customer c002. Use a subquery.
+SELECT city
+FROM agents
+WHERE aid IN 
+	(SELECT aid 
+	 FROM orders
+	 WHERE cid = 'c002')
+ORDER BY city;
+
+-- 2. Get the cities of agents booking an order for customer c002. Use joins.
+SELECT a.city
+FROM orders o, agents a , customers c
+WHERE c.aid = o.aid 
+	AND o.cid = c.cid 
+	AND c.cid = 'c002'
+
+-- 3. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use subqueries.
+SELECT cid, name
+FROM customers
+WHERE cid NOT IN (SELECT cid
+		FROM orders
+		WHERE aid = 'a03'
+		)
+
+HELPP****
+-- 4. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use joins.
+SELECT distinct pid
+FROM orders o, agents a, customers c
+WHERE o.aid = a.aid 
+	AND o.cid = c.cid 
+	AND c.city = 'Kyoto'
+ORDER by pid asc
+
+select distinct pid
+from orders
+where aid in (select aid
+				from orders o,
+					customers c
+				where o.cid = c.cid 
+				and c.city = 'Kyoto')
+order by pid asc
+-- 5. Get the names of customers who have never placed an order. Use a subquery.
+SELECT c.name 
+FROM customers c
+WHERE c.cid NOT IN (SELECT cid 
+					FROM orders
+					)
+
+--6.Get	the	names of customers who have never placed an	order. Use an outer join.
+SELECT c.name 
+FROM customers c full outer join orders o  on c.cid = o.cid 
+WHERE o.cid IS null 
+
+HELPP*****
+--7.Get the	names of customers who placed at least one order through an	agent in their city, along with	those agent(s) names.
+SELECT c.name, a.name
+FROM customers c, agents a, orders o 
+WHERE c.cid = o.cid
+	AND a.aid = o.aid 
+	AND 
+--8. Get the names	of customers and agents in the same	city, along	with the name of the city, regardless of whether or not the	customer has ever	
+--placed an	order with	that agent.
+SELECT DISTINCT c.name, a.name, c.city
+FROM customers c, agents a
+WHERE c.city = a.city 
+
+--9.Get	the	name and city of customers who live	in the city	where THE least number of products are made.
+
+SELECT c.name, c.city 
+FROM    customers c 
+WHERE  city IN ( SELECT city 
+				 FROM products
+				 group by city
+				 order by count(*) asc
+				 limi)
+
+
+
+
+--10.Get the name and city	of customers who live in a city	where the most number of products are made.
+SELECT c.name, c.city
+FROM customers c INNER JOIN products p ON c.city = p.city
+WHERE p.quantity IN
+(SELECT MAX(quantity)
+FROM products);
+
+--11. Get the name and city of customers who live in any city where the most number of products are made.
+
+-- 12. List the products whose priceUSD is above the average priceUSD.
+
+-- 13. Show the customer name, pid ordered, and the dollars for all customer orders, sorted by dollars from high to low.
+
+-- 14. Show all customer names (in order) and their total ordered, and nothing more. Use coalesce to avoid showing NULLs.
+
+--15. Show the names of all customers who bought products from agents based in New York along with the names of
+--     the products they ordered, and the names of the agents who sold it to them.
+
+-- 16. Write a query to check the accuracy of the dollars column in the Orders table. This means calculating
+--     Orders.dollars from other data in other tables and then comparing those values to the values in Orders.dollars.
+-- This query, if it finds any errors, displays the ordno of where the error occurs, the given price and calculated price.
+
+- - - - - Fix----
+---- 17. Create an error in the dollars column of the Orders table so that you can verify your accuracy checking query.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
