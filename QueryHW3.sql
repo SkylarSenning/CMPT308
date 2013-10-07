c9e8d3cd3aaa72786ae96591d9f9a12a7fc718f8
Skylar Senning <Skylar.Senning1@marist.edu>
2013-10-07 00:06:36 -0400

    11-17 finished
    Almost all the questions are done….just need to do some editing

--- QueryHW3.sql
+++ QueryHW3.sql
@@ -22,15 +22,18 @@ WHERE cid NOT IN (SELECT cid
 		WHERE aid = 'a03'
 		)
 
-HELPP****
+
 -- 4. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use joins.
-SELECT distinct pid
-FROM orders o, agents a, customers c
-WHERE o.aid = a.aid 
-	AND o.cid = c.cid 
+SELECT distinct o2.pid
+FROM  orders o1, 
+	  orders o2,
+	  agents a, 
+	  customers c
+WHERE o1.aid = o2.aid 
+	AND o1.cid = c.cid 
 	AND c.city = 'Kyoto'
 ORDER by pid asc
-
+--
 select distinct pid
 from orders
 where aid in (select aid
@@ -39,6 +42,7 @@ where aid in (select aid
 				where o.cid = c.cid 
 				and c.city = 'Kyoto')
 order by pid asc
+
 -- 5. Get the names of customers who have never placed an order. Use a subquery.
 SELECT c.name 
 FROM customers c
@@ -51,56 +55,93 @@ SELECT c.name
 FROM customers c full outer join orders o  on c.cid = o.cid 
 WHERE o.cid IS null 
 
-HELPP*****
+
 --7.Get the	names of customers who placed at least one order through an	agent in their city, along with	those agent(s) names.
-SELECT c.name, a.name
+SELECT distinct c.name as "Customer", a.name as "Agent"
 FROM customers c, agents a, orders o 
 WHERE c.cid = o.cid
 	AND a.aid = o.aid 
-	AND 
+	AND c.city = a.city
+
 --8. Get the names	of customers and agents in the same	city, along	with the name of the city, regardless of whether or not the	customer has ever	
 --placed an	order with	that agent.
-SELECT DISTINCT c.name, a.name, c.city
+SELECT DISTINCT c.name AS "Customer", a.name AS "Agent", c.city AS "City"
 FROM customers c, agents a
 WHERE c.city = a.city 
 
 --9.Get	the	name and city of customers who live	in the city	where THE least number of products are made.
-
-SELECT c.name, c.city 
-FROM    customers c 
-WHERE  city IN ( SELECT city 
-				 FROM products
-				 group by city
-				 order by count(*) asc
-				 limi)
-
-
-
+SELECT c.name, c.city
+FROM customers c 
+WHERE c.city  IN (SELECT city
+				FROM products p 
+				GROUP BY p.city
+				ORDER BY count(quantity) ASC 
+				LIMIT 1);
 
 --10.Get the name and city	of customers who live in a city	where the most number of products are made.
 SELECT c.name, c.city
-FROM customers c INNER JOIN products p ON c.city = p.city
-WHERE p.quantity IN
-(SELECT MAX(quantity)
-FROM products);
+FROM customers c 
+WHERE c.city  IN (SELECT city
+				FROM products p
+				GROUP BY p.city
+				ORDER BY count(quantity) DESC
+				LIMIT 1);
 
 --11. Get the name and city of customers who live in any city where the most number of products are made.
+SELECT c.name, c.city
+FROM customers c 
+WHERE c.city  IN (SELECT city
+				FROM products
+				WHERE quantity = (SELECT max(quantity)
+									FROM products))
+ORDER BY c.city ASC;
 
 -- 12. List the products whose priceUSD is above the average priceUSD.
+SELECT name
+FROM products p
+WHERE priceUSD > (SELECT avg(priceUSD)
+					FROM products)
+ORDER BY name ASC;
 
 -- 13. Show the customer name, pid ordered, and the dollars for all customer orders, sorted by dollars from high to low.
+SELECT DISTINCT c.name, o.pid, o.dollars 
+FROM customers c, orders o, products p
+WHERE c.cid = o.cid 
+ORDER BY dollars DESC;
 
 -- 14. Show all customer names (in order) and their total ordered, and nothing more. Use coalesce to avoid showing NULLs.
+SELECT c.name AS "Customer" , coalesce(sum(o.dollars), 0.00)
+FROM customers c
+left join on orders on c.cid= o.cid
+GROUP by cid
+ORDER BY name ASC
 
 --15. Show the names of all customers who bought products from agents based in New York along with the names of
 --     the products they ordered, and the names of the agents who sold it to them.
+SELECT c.name, p.name, a.name
+FROM customers c, 
+	 products p, 
+	 agents a, 
+	 orders o
+WHERE c.cid = o.cid 
+	AND p.pid = o.pid
+	AND a.aid = o.aid 
+	AND p.city = 'New York'
 
 -- 16. Write a query to check the accuracy of the dollars column in the Orders table. This means calculating
 --     Orders.dollars from other data in other tables and then comparing those values to the values in Orders.dollars.
 -- This query, if it finds any errors, displays the ordno of where the error occurs, the given price and calculated price.
+SELECT o.ordno AS "Order Number" , o.dollars "Price" , ((p.priceUSD * o.qty) - (c.discount / 100) * (p.priceUSD * o.qty)) AS "Price Check"
+FROM orders o, products p, customers c 
+WHERE o.cid = c.cid 
+	AND o.pid = p.pid
+ORDER BY o.ordno ASC;
 
-- - - - - Fix----
 ---- 17. Create an error in the dollars column of the Orders table so that you can verify your accuracy checking query.
+UPDATE Orders
+SET dollars = dollars - 1
+WHERE ordno = 1017;
+
 
 
 
